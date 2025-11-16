db695a97b8fd48111915b55ab4297095fb853867
Farid Yusof <faridyusof727@gmail.com>
2025-11-16 18:24:43 +0800

    docs(printer): add interface documentation and method comments

--- printer.go
+++ printer.go
@@ -1,6 +1,9 @@
 package main
 
+// Printer defines the interface for formatting and outputting changelog data.
 type Printer interface {
+	// MapData processes and maps the provided tag information for output.
 	MapData(tags []*TagInfo)
+	// Print outputs the formatted changelog data.
 	Print()
 }
